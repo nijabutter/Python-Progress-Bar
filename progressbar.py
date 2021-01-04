ed7613df212d261915ab2c5513180dd419c2c67f
nijabutter <nijabutter@gmail.com>
2021-01-04 01:39:34 +0000

    add borders

--- progressbar.py
+++ progressbar.py
@@ -1,15 +1,18 @@
 class ProgressBar:
-    def __init__(self, value=0, maximum=100, length=25, empty='░', fill='█'):
+    def __init__(self, value=0, maximum=100, length=25, empty='░', fill='█', border=("","")):
         self.value = value
         self.maximum = maximum
         self.length = length
         self.empty = empty
         self.fill = fill
+        self.border = border
+        if type(self.border) != tuple or len(self.border) != 2:
+        	self.border = ("", "")
     def get(self):
         percent = float(self.value) / float(self.maximum)
         fill = int(percent * self.length)
         empty = self.length - fill
-        return (self.fill * fill) + (self.empty * empty) + " " + str(int(percent*100)).rjust(3) + "%"
+        return self.border[0] + (self.fill * fill) + (self.empty * empty) + self.border[1] + " " + str(int(percent*100)).rjust(3) + "%"
     def set(self, value):
         self.value = value
         if self.value > self.maximum:
